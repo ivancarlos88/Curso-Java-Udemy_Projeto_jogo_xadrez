fd783a515a0d776493e7b50b5c96d0210880f2e2
ivancarlos88 <ivancarlos.ms.21@gmail.com>
2024-02-29 19:43:50 -0300

    Tratando exceções e limpando tela

--- src/application/UI.java
+++ src/application/UI.java
@@ -29,6 +29,11 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	public static void clearScreen() {
+		System.out.println("\033[H\033[2J");
+		System.out.flush();
+	}
+	
 	public static ChessPosition readChessPosition(Scanner ler) {
 		try {
 			String s = ler.nextLine();
@@ -36,7 +41,7 @@ public class UI {
 			int row = Integer.parseInt(s.substring(1));
 			return new ChessPosition(column, row);
 		} catch (RuntimeException e) {
-			throw new InputMismatchException("Error: Entrada invalida");
+			throw new InputMismatchException("Error: Entrada invalida(somente de A1 ao H8)");
 		}
 	}
 
